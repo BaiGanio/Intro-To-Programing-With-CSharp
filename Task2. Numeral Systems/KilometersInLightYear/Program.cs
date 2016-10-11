f1b937a6503fefc601d8efebcb0fbcfb77e551ba
BaiGanio <big_l@abv.bg>
2016-10-11 12:49:59 +0300

    Added on 10/11/2016 12:50

--- Task2. Numeral Systems/KilometersInLightYear/Program.cs
+++ Task2. Numeral Systems/KilometersInLightYear/Program.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace KilometersInLightYear
 {
     class Program
     {
         static void Main(string[] args)
         {
+   
         }
     }
 }
