9844657b4adfdb1b600ca2b010ceefc1ecde0f89
baiganio <big_l@abv.bg>
2017-10-07 16:49:06 +0300

    Included week3 In class demo solution with object example.

---
+++ Week 3/In class/Week3 Demos/PlayWavFormatMusic/Startup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Media;
+using System.Text;
+
+namespace PlayWavFormatMusic
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("ХаФана бийч, бейби ;)");
+            var simpleSound = new SoundPlayer("../../wav-files/nj.wav");
+            simpleSound.Play();
+
+            Student peshko = new Student()
+            {
+                Name = "Пешко",
+                University = "ХаФана бийч, бейби ;)",
+                FacultyNumber = 9088888,
+                IsGraduated = false,
+                OverallScore = 99.99
+            };
+
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("Greetings from the best party place in the world!");
+            Console.WriteLine("Любен Киков");
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("Press any key to stop the program!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Week 3/In class/Week3 Demos/PlayWavFormatMusic/Student.cs
@@ -0,0 +1,25 @@
+﻿namespace PlayWavFormatMusic
+{
+    public class Student
+    {
+        public string Name { get; set; }
+
+        public string University { get; set; }
+
+        public long FacultyNumber { get; set; }
+
+        public bool IsGraduated { get; set; }
+
+        public double OverallScore { get; set; }
+
+        public string Introduce()
+        {
+            return
+                $"Hello, I'm {this.Name}. " +
+                $"\nAnd I study in {this.University} university! " +
+                $"\nFaculty number: {this.FacultyNumber} " +
+                $"\nIs graduated: { this.IsGraduated} " +
+                $"\nOverall score: % {this.OverallScore}";
+        }
+    }
+}
