d00cfa02c3f3f0df3ac36d03428ff7ab1673c60a
baiganio <big_l@abv.bg>
2017-10-17 21:18:08 +0300

    Some changes in old exam files.

--- Week 3/In class/Week3 Demos/PlayWavFormatMusic/Startup.cs
+++ Week 3/In class/Week3 Demos/PlayWavFormatMusic/Startup.cs
@@ -8,30 +8,36 @@ namespace PlayWavFormatMusic
     {
         static void Main(string[] args)
         {
-            Console.OutputEncoding = Encoding.UTF8;
-            Console.WriteLine("ХаФана бийч, бейби ;)");
-            var simpleSound = new SoundPlayer("../../wav-files/nj.wav");
-            simpleSound.Play();
-
-            Student peshko = new Student()
+            for (int i = 0; i < 10; i++)
             {
-                Name = "Пешко",
-                University = "ХаФана бийч, бейби ;)",
-                FacultyNumber = 9088888,
-                IsGraduated = false,
-                OverallScore = 69.69
-            };
+                Console.WriteLine("Hi");
+            }
 
-            Console.WriteLine();
-            Console.WriteLine("--------------------------------------");
-            Console.WriteLine("Greetings from the best party place in the world!");
-            Console.WriteLine("Любен Киков");
-            Console.WriteLine("--------------------------------------");
-            Console.WriteLine("Week 3 - Course intro to programming with c#");
-            Console.WriteLine();
-            Console.WriteLine("--------------------------------------");
-            Console.WriteLine("Press any key to stop the program!");
-            Console.ReadKey();
+
+            //Console.OutputEncoding = Encoding.UTF8;
+            //Console.WriteLine("ХаФана бийч, бейби ;)");
+            //var simpleSound = new SoundPlayer("nj.wav");
+            //simpleSound.Play();
+
+            //Student peshko = new Student()
+            //{
+            //    Name = "Пешко",
+            //    University = "ХаФана бийч, бейби ;)",
+            //    FacultyNumber = 9088888,
+            //    IsGraduated = false,
+            //    OverallScore = 69.69
+            //};
+
+            //Console.WriteLine();
+            //Console.WriteLine("--------------------------------------");
+            //Console.WriteLine("Greetings from the best party place in the world!");
+            //Console.WriteLine("Любен Киков");
+            //Console.WriteLine("--------------------------------------");
+            //Console.WriteLine("Week 3 - Course intro to programming with c#");
+            //Console.WriteLine();
+            //Console.WriteLine("--------------------------------------");
+            //Console.WriteLine("Press any key to stop the program!");
+            //Console.ReadKey();
         }
     }
 }
---
+++ Week 4/In class/week4-demos/HandyProgram/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace HandyProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool correctInputNumber = false;
+            while (correctInputNumber == false)
+            {
+                try
+                {
+                    // 1. Take main number
+                    Console.WriteLine("Enter number which will be used for some calculations: ");
+                    int mainNumber = int.Parse(Console.ReadLine());
+
+                    bool correctInputNumbersLenght = false;
+                    while (correctInputNumbersLenght == false)
+                    {
+                        try
+                        {
+                            // 2. Take number(which will be the lenght) of the current input
+                            Console.WriteLine("How much nubers do you want to enter: ");
+                            int numbersInputLenght = int.Parse(Console.ReadLine());
+                        }
+                        catch (Exception exx)
+                        {
+                            Console.WriteLine(exx.Message);
+                            correctInputNumbersLenght = false;
+                        }
+                        correctInputNumbersLenght = true;
+                    }
+
+                    correctInputNumber = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    correctInputNumber = false;
+                }
+                Console.WriteLine("-----------------------------------------------");
+            }
+
+           
+
+
+
+
+
+
+            // 3. Store each entered number into data collection
+            // 4. Do some calculation
+            //     - a)
+            //     - b)
+            //int counter = 10;
+            //Console.Write("Working");
+            //while (counter > 0)
+            //{
+            //    Console.Write(".");
+            //    Thread.Sleep(500);
+            //    counter--;
+            //}
+            //Console.WriteLine();
+            //Thread.Sleep(1000);
+            //Console.WriteLine("Calculations are completed successfuly!");
+            //Thread.Sleep(1000);
+            //Console.WriteLine("Showing the results: ");
+            //Thread.Sleep(1000);
+            //Console.ReadKey();
+        }
+    }
+}
