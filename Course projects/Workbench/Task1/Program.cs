69637dd1720c4d8cd33edc7c4d9d4f8ec7648b04
BaiGanio <lubenkikov@gmail.com>
2017-11-20 15:36:27 +0200

    Created Course projects folder and included Workbench project.

---
+++ Course projects/Workbench/Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            STD1A();
+            STD3A();
+        }
+
+        private static void STD3A()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void STD1A()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
