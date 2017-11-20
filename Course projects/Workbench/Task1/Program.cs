122d30d14ca064be8fa2e45f177bcafc199330e5
BaiGanio <lubenkikov@gmail.com>
2017-11-20 15:39:19 +0200

    Some first words said.

--- Course projects/Workbench/Task1/Program.cs
+++ Course projects/Workbench/Task1/Program.cs
@@ -12,12 +12,12 @@ namespace Task1
 
         private static void STD3A()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Hello STD 1A!");
         }
 
         private static void STD1A()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Hello STD 3A!");
         }
     }
 }
