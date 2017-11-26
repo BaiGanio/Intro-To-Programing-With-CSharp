670510fa9b0379c673900ced508b01006448db0f
Tatyana Andreeva <tatyana144@abv.bg>
2017-11-26 15:01:50 +0200

    Included Task4.

---
+++ Course projects/Workbench/Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            STD1A();
+            STD3A();
+        }
+        public static void STD3A()
+        {
+            _1701681070_TatyanaAndreeva.CountingPoints();
+        }
+        public static void STD1A()
+        {
+        }
+
+    }
+}
---
+++ Course projects/Workbench/Task4/_1701681070_TatyanaAndreeva.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class _1701681070_TatyanaAndreeva
+    {
+        public static void CountingPoints()
+        {
+                int count = 0;
+                for (int x = -13; x <= 13; x++)
+                {
+                    for (int y = -16; y <= 16; y++)
+                    {
+                        double A = Math.Pow(Math.Abs(x), 2) / 169 + Math.Pow(Math.Abs(y), 2) / 256;
+                        if (A < 1)
+                        {
+                            Console.WriteLine("A[{0},{1}]", x, y);
+                            count++;
+                        }
+                    }
+                }
+                Console.WriteLine("Броят на точките в елипсата е {0}", count);
+        }
+    }
+}
