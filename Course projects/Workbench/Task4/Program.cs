7f2090da550ab2b366feb94f9409d55be138c3e1
BaiGanio <lubenkikov@gmail.com>
2017-11-29 16:15:19 +0200

    Removed some code.

--- Course projects/Workbench/Task4/Program.cs
+++ Course projects/Workbench/Task4/Program.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Task4.STD1A;
-using Task4.STD3A;
 
 namespace Task4
 {
@@ -12,18 +11,7 @@ namespace Task4
     {
         static void Main(string[] args)
         {
-            STD1A();
-            STD3A();
-        }
-
-        public static void STD1A()
-        {
-            _1701681009_Lusian_Manolov.DisplayFigure_87D();
-        }
-
-        public static void STD3A()
-        {
-            _1701681070_TatyanaAndreeva.CountingPoints();
+           
         }
     }
 }
