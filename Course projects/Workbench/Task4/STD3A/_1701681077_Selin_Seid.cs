5792f27ea5ec777fc96f03e76145e8e61fee4e8f
Selin Seid <incredible15@abv.bg>
2017-11-29 17:38:45 +0200

    Edited some mistakes from task 3

--- Course projects/Workbench/Task4/STD3A/_1701681077_Selin_Seid.cs
+++ Course projects/Workbench/Task4/STD3A/_1701681077_Selin_Seid.cs
@@ -10,11 +10,13 @@ namespace Task4.STD3A
     {
         public static void Triangle_Figure_Using_Stars_87_d()
         {
-            for (int i = 13; i >= 1; i--)
+            for (int width = 13; width >= 1; width--)
             {
-                Console.Write("{0," + i + "}", "*");
-                for (int j = 1; j <= 13 - i; j++)
+                Console.Write("{0," + width + "}", "*");
+                for (int j = 1; j <= 13 - width; j++)
+                {
                     Console.Write(" *");
+                }
                 Console.WriteLine();
             }
         }
