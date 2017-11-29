59bcbe4c856cd0a16cf13262286e234fa2bcc47e
Selin Seid <incredible15@abv.bg>
2017-11-29 15:33:01 +0200

    Added task 3.

---
+++ Course projects/Workbench/Task3/_1701681077_Selin_Seid.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public static class _1701681077_Selin_Seid
+    {
+        public static void Calculate_InterestRate_65()
+        {
+            Console.WriteLine("Enter the number of month:");
+            int month = int.Parse(Console.ReadLine());
+            double intRate = 0;
+            switch (month)
+            {
+                case 1:
+                case 12:
+                    intRate = 3.8;
+                    break;
+                case 2:
+                case 3:
+                case 4:
+                    intRate = 3.5;
+                    break;
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                    intRate = 4.1;
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    intRate = 4.9;
+                    break;
+                default:
+                    Console.WriteLine("Unvalid number of month!");
+                    break;
+            }
+            Console.WriteLine("The interest rate is: " + intRate);
+        }
+    }
+}
