a39bdef02359956a6868759d02e1e4c4ca3733b7
BaiGanio <exceptionhelper@gmail.com>
2018-02-19 01:02:42 +0200

    Few files remoced

--- Контролно № 2 - workbench/JupiterPancakes/Program.cs
+++ Контролно № 2 - workbench/JupiterPancakes/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,15 @@ namespace JupiterPancakes
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(ConvertCustomIdToInt("afeccbae-8900-487f-b7f3-1ad60166aea7"));
+        }
+
+        public static int ConvertCustomIdToInt(string id)
+        {
+            MD5 md5Hasher = MD5.Create();
+            var hashed = md5Hasher.ComputeHash(Encoding.UTF8.GetBytes(id));
+            int ivalue = BitConverter.ToInt32(hashed, 0);
+            return Math.Abs(ivalue);
         }
     }
 }
