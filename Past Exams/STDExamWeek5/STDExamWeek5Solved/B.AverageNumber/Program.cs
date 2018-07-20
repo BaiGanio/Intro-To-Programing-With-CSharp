83a4c34e3c7b7f7f25b8da84cb7c825c34a91401
Sunny <sunni98@abv.bg>
2018-07-20 15:15:26 +0300

    Added past exams tasks pdfs and replaced solution archives with actual solutions

---
+++ Past Exams/STDExamWeek5/STDExamWeek5Solved/B.AverageNumber/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B.AverageNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare and initialize the variables we need.
+            int number;
+            int evenNumbersCount = 0;
+            double evenNumbersSum = 0;
+            bool inputIsNumber;
+
+            Console.WriteLine("Enter some numbers. When you're done, enter 0.");
+
+            // Make a do-while loop, which will repeat itself until the input number is 0.
+            do
+            {
+                Console.WriteLine("Enter a number: ");
+                // If the input is a number, inputIsNumber will be true. If it's not the loop will iterate one more time.
+                inputIsNumber = int.TryParse(Console.ReadLine(), out number);
+
+                // Check if the input number is between 1 and 255 and if it is even.
+                if ((number >= 1 && number <= 255) && (number % 2 == 0))
+                {
+                    evenNumbersSum += number;
+                    evenNumbersCount++;
+                }
+            } while (number != 0 || !inputIsNumber);
+            
+            // Check if there are any even numbers between 1 and 255. If there aren't, display a suitable message.
+            // If there are, calculate the average and display it.
+            if (evenNumbersCount == 0)
+            {
+                Console.WriteLine("There aren't any even numbers between 1 and 255.");
+            }
+            else
+            {
+                double evenNumbersAverage = evenNumbersSum / evenNumbersCount;
+                Console.WriteLine("The average of the even numbers between 1 and 255 is: " + evenNumbersAverage);
+            }
+        }
+    }
+}
