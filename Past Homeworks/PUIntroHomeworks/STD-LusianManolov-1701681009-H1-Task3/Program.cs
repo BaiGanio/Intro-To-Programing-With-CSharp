750bd6be756b156ad8ccadadb41219c84d5835e9
Sunny <sunni98@abv.bg>
2018-09-07 15:12:37 +0300

    United tasks from Homework 2017 into one solution.

---
+++ Past Homeworks/PUIntroHomeworks/STD-LusianManolov-1701681009-H1-Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace STD_LusianManolov_1701681009_H1_Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = Math.PI; // This is how you declare and initialize a constant.
+            Console.WriteLine("How old are you?");
+            // We take the logic from the previous task to calculate age after 10 years.
+            int age = int.Parse(Console.ReadLine());
+            int ageAfter10 = age + 10;
+            // Then we calculate the unique number using the given formula.
+            Console.WriteLine("Enter last 4 digit of your faculty number");
+            int facultyNumberLast4Digits = int.Parse(Console.ReadLine());
+            double uniqueNumber = (ageAfter10 * facultyNumberLast4Digits) / pi;
+            // Finally, we output the results.
+            Console.WriteLine($"You will be {ageAfter10} years old after 10 years. And your unique number is " +
+                $"{Math.Round(uniqueNumber, 4)}.");
+        }
+    }
+}
---
+++ Past Homeworks/PUIntroHomeworks/STD-LusianManolov-1701681009-H5-Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace STD_LusianManolov_1701681009_H5_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Declare and initialize the variables we need.
+               BONUS: Figure a way to validate that the input is correct */
+            Console.WriteLine("Enter 3 numbers.");
+            Console.Write("Enter the first number: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.Write("Enter the second number: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+            Console.Write("Enter the third number: ");
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            // Call the method which will compare the 3 numbers
+            Console.WriteLine("The biggest number is: " + GetMax(firstNumber, secondNumber, thirdNumber));
+        }
+
+        /* Don't forget it is good practise to put all other methods after the Main() method. 
+           Here the method will return a number (the greatest one) so that's why
+           it is declared as int */
+        static int GetMax(int num1, int num2, int num3)
+        {
+            // Compare the 3 numbers and return the greatest one
+            if (num1 > num2 && num1 > num3)
+            {
+                return num1;
+            }
+            else if (num2 > num1 && num2 > num3)
+            {
+                return num2;
+            }
+            else return num3;
+        }
+    }
+}
