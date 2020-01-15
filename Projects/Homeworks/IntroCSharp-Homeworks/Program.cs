b5915c23d06482b43e3c62c5c93c12c17a31eaa4
Lyuben Kikov <exceptionhelper@gmail.com>
2020-01-15 20:07:02 +0200

    Added Homeworks.

---
+++ Projects/Homeworks/IntroCSharp-Homeworks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IntroCSharp_Homeworks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
