a9d5eb21d4307e5593561c75f115fd2c692fb05b
Lyuben Kikov <exceptionhelper@gmail.com>
2020-01-15 20:19:54 +0200

    Stuffs.

---
+++ Projects/IntroCSharp-Homeworks/HelloProgram/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+
+            string name = "Lyuben";
+            Console.WriteLine(name);
+        }
+    }
+}
